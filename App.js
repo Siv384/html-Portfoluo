e5eda2b6224bbed028c5fc167d0736904c8aec04
Siv384 <158508499+Siv384@users.noreply.github.com>
2024-02-02 00:11:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Imp from './Imp';
+function App() {
+  return (
+    <div className="App">
+   <Imp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Imp.js
@@ -0,0 +1,200 @@
+import React, { Component } from 'react'
+
+export default class Imp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Book</h1>
+        <h2>Chapter 1</h2>
+        <h3>Section 1</h3>
+        <h3>Section 2</h3>
+        <h2>Chapter 2</h2>
+        <h3>Section 1</h3>
+        <h4>Diagram 1</h4>
+        <h2>Chapter 3</h2>
+        <h3>Section 1</h3>
+        <h3>Section 2</h3>
+
+
+        <p> First paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Arcu cursus vitae congue mauris. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Nisi est sit amet
+          facilisis magna. Diam sit amet nisl suscipit adipiscing bibendum est ultricies integer. Quis ipsum suspendisse ultrices
+          gravida dictum fusce ut. Euismod elementum nisi quis eleifend. Habitant morbi tristique senectus et. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer. Viverra orci sagittis eu volutpat odio facilisis mauris sit. Nisi quis
+          eleifend quam adipiscing. Neque convallis a cras semper auctor neque vitae. Magna fermentum iaculis eu non. Vivamus arcu
+          felis bibendum ut tristique et. Justo nec ultrices dui sapien eget mi. In vitae turpis massa sed elementum tempus. Eu
+          facilisis sed odio morbi quis commodo. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae.</p>
+
+        <p>Second paragraph. Suscipit adipiscing bibendum est ultricies. Tortor aliquam nulla facilisi cras fermentum. Eget aliquet nibh praesent
+          tristique magna. In hac habitasse platea dictumst vestibulum. Ornare quam viverra orci sagittis eu. Sit amet est
+          placerat in. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Turpis in eu mi bibendum neque egestas
+          congue. Enim eu turpis egestas pretium aenean pharetra magna ac placerat. Ultrices sagittis orci a scelerisque purus
+          semper eget duis at. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Condimentum
+          lacinia quis vel eros donec ac odio.</p>
+
+        <p>Third paragraph. Nisl purus in mollis nunc sed id semper risus. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Ridiculus mus
+          mauris vitae ultricies leo integer malesuada nunc. In tellus integer feugiat scelerisque. Lectus mauris ultrices eros in
+          cursus turpis massa. Sollicitudin ac orci phasellus egestas. Massa massa ultricies mi quis hendrerit dolor. Quam
+          elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Iaculis nunc sed augue lacus viverra. Id ornare
+          arcu odio ut sem nulla pharetra. Amet luctus venenatis lectus magna fringilla urna porttitor. Eu nisl nunc mi ipsum
+          faucibus vitae aliquet nec ullamcorper. Nunc mattis enim ut tellus elementum sagittis. Mauris augue neque gravida in
+          fermentum et sollicitudin. Pellentesque habitant morbi tristique senectus. Tristique senectus et netus et. Turpis
+          egestas sed tempus urna et pharetra pharetra. Feugiat vivamus at augue eget arcu dictum varius duis at. Lacus sed
+          viverra tellus in hac habitasse platea dictumst vestibulum. Nisl condimentum id venenatis a condimentum vitae sapien.</p>
+        <div>
+          <h1>William Blake </h1>
+
+          <h2>17 south molton street</h2>
+          <h2>London</h2>
+          <h2>W1K 5QT</h2>
+          <h2>UK</h2><hr />
+
+
+          <p>William Blake (28 November 1757 – 12 August 1827) was an English poet, painter, and printmaker. Largely unrecognised
+            during his life, Blake is now considered a seminal figure in the history of the poetry and visual art of the Romantic
+            Age. What he called his "prophetic works" were said by 20th-century critic Northrop Frye to form "what is in proportion
+            to its merits the least read body of poetry in the English language".[2] His visual artistry led 21st-century critic
+            Jonathan Jones to proclaim him "far and away the greatest artist Britain has ever produced".[3] In 2002, Blake was
+            placed at number 38 in the BBC's poll of the 100 Greatest Britons.[4] While he lived in London his entire life, except
+            for three years spent in Felpham,[5] he produced a diverse and symbolically rich collection of works, which embraced the
+            imagination as "the body of God"[6] or "human existence itself".[7]</p>
+
+          <p>Although Blake was considered mad by contemporaries for his idiosyncratic views, he is held in high regard by later
+            critics for his expressiveness and creativity, and for the philosophical and mystical undercurrents within his work. His
+            paintings and poetry have been characterised as part of the Romantic movement and as "Pre-Romantic".[8] In fact, he has
+            been said to be "a key early proponent of both Romanticism and Nationalism".[9] A committed Christian who was hostile to
+            the Church of England (indeed, to almost all forms of organised religion), Blake was influenced by the ideals and
+            ambitions of the French and American revolutions.[10][11] Though later he rejected many of these political beliefs, he
+            maintained an amiable relationship with the political activist Thomas Paine; he was also influenced by thinkers such as
+            Emanuel Swedenborg.[12] Despite these known influences, the singularity of Blake's work makes him difficult to classify.
+            The 19th-century scholar William Michael Rossetti characterised him as a "glorious luminary",[13] and "a man not
+            forestalled by predecessors, nor to be classed with contemporaries, nor to be replaced by known or readily surmisable
+            successors".[14]</p>
+        </div>
+        <div>
+          <h1>The Best Movies According to Angela</h1>
+          <h2>My top 3 movies of all-time</h2><hr />
+          <h3>Spirited Away</h3>
+          <p>This is my favourite anime. I love the beautiful images</p>
+          <h3>Ex Machin</h3>
+          <p>Really cool sci-fi movie</p>
+          <h3>Drive</h3>
+          <p>Super beautiful film. Really artistic</p>
+        </div>
+        <div>
+          <h1>Angela's Cinnamon Roll Recipe</h1>
+
+          <h2>Ingredients</h2>
+
+          <h3>For the dough:</h3>
+          <ul>
+            <li> ¾ cup warm milk</li>
+            <li>2 ¼ teaspoons yeast</li>
+            <li> ¼ cup granulated sugar</li>
+            <li>1 egg plus 1 egg yolk</li>
+            <li> ¼ cup butter</li>
+            <li>3 cups bread flour</li>
+          </ul>
+          <h3>For the filling:</h3>
+          <ul>
+            <li>2/3 cup dark brown sugar</li>
+            <li>1 ½ tablespoons ground cinnamon</li>
+            <li>  ¼ cup butter</li>
+          </ul>
+          <h2>Instructions</h2>
+          <ol>
+            <li>Mix the milk with the yeast, sugar, eggs.</li>
+            <li>Melt the butter and add to the mixture.</li>
+            <li>Add in the flour and mix until combined into a dough.</li>
+            <li>Knead the dough for 10 minuites.</li>
+            <li>Transfer the dough into a large bowl and cover with plastic wrap. Leave it somewhere to rise for 2 hours.</li>
+            <li>After the dough has doubled in size, roll it out into a large rectangle.</li>
+            <li>Melt the butter for the filling and mix in the sugar and cinnamon.</li>
+            <li>Spread the filling onto the dough then roll the dough into a swiss roll.</li>
+            <li>  Cut the roll into 3cm sections and place flat into a baking tray.</li>
+            <li>Pre-heat the oven to 350F or 180C, then bake the rolls for 20-25min until lightly brown.</li>
+          </ol>
+        </div>
+        <div>
+          <ul>
+            <li>A</li>
+            <li>B</li>
+            <ol>
+              <li>B1</li>
+              <li>B2</li>
+              <ul>
+                <li>B2a</li>
+                <li>B2b</li>
+                <ul>
+                  <li>B2aa</li>
+                  <li>B2ab</li>
+                </ul>
+                <li>B2c</li>
+              </ul>
+              <li>B3</li>
+              <ol>
+                <li>B31</li>
+                <li>B32</li>
+              </ol>
+            </ol>
+            <li>C</li>
+          </ul>
+        </div>
+        <div>
+          <h1>My top 5 Favourite Websites</h1>
+          <ol start="6">
+            <li><a href='Product Hunt'>Product Hunt</a></li>
+            <li><a href='Smash the Walls'>Smash the Walls</a></li>
+            <li><a href='Wordle'>Wordle</a></li>
+            <li><a href='Hacker Typer'>Hacker Typer</a></li>
+            <li><a href='Stellarium'>Stellarium</a></li>
+            <li><a href='https://www.google.com/'>google</a></li>
+          </ol>
+        </div>
+
+        <div>
+          <h1>Animals are best friends to humans</h1>
+          <img Src="https://raw.githubusercontent.com/appbrewery/webdev/main/kitten.jpeg" alt="nature with water falls"/>
+          <h1>Health problems are more with leaving with pigs</h1>
+          <img Src="https://raw.githubusercontent.com/appbrewery/webdev/main/puppy.gif" alt="pig dig in sand"/>
+        </div><hr></hr>
+        <div>
+          <h1>It's My Birthday!</h1>
+          <h2>on the 5th May</h2>
+          <img src="https://raw.githubusercontent.com/appbrewery/webdev/main/birthday-cake3.4.jpeg" alt="cake pic"/>
+          <h3>What to bring:</h3>
+          <ul>
+            <li>Baloons(I Love Baloons)</li>
+            <li>Cake(I'm really good at eating)</li>
+            <li>An appetite (There will be lots of food)</li>
+          </ul>
+          <h3>This is where you need to go:</h3>
+        <a href="https://www.google.com/maps/@22.7803136,86.163456,14z?entry=ttu"><strong>Google maps</strong></a>
+        </div>
+        <div>
+          <h1>Angela Yu's Portfoluo</h1>
+          <h2>I'm a Web Developer</h2>
+          <hr/>
+          <h3><a href="www.Movie Ranking Project">Movie Ranking Project </a></h3>
+          <h3>The Best Movie According to Angela</h3>
+          <h4>My top 3 movies of all-time.</h4>
+          <hr/>
+          <h5>Spirited Away</h5>
+          <p>This is my favourite anime. I Love the beautiful images</p>
+          <h5>Ex Machina</h5>
+          <p>Really cool sci-fi movie.</p>
+          <h5>Drive</h5>
+          <p>Super beautiful film. Really artistic</p>
+          <h3><a href="Birthday Invite Project">Birthday Invite Project</a></h3>
+          <h4>It's My Birthday!</h4>
+          <h5>On the 5th May</h5>
+          <img src="https://raw.githubusercontent.com/appbrewery/webdev/main/birthday-cake3.4.jpeg" alt="cake pic"/>
+          <p>What to bring:</p>
+
+
+        </div>
+
+      </div>
+    )
+  }
+}
